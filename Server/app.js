06e7cd9d54ec8d069a6a9c932d43b94c3c6c74db
Noel Lazaro <44674847+noelj@users.noreply.github.com>
2024-01-12 17:03:35 -0500

    Upgrade

--- Server/app.js
+++ Server/app.js
@@ -4,10 +4,21 @@ const errorHandler = require('./middleware/erroHandler');
 
 const dotenv =require('dotenv').config()
 
+const bodyparse = require('body-parse');
+
+const mongoose = require('mongoose')
+
+const uri = process.env.URI;
 
 const port = process.env.PORT||3000 ;
 
 const app = express();
+
+
+
+app.use(bodyparser.urlencoded({extended:true}))
+
+app.use(bodyparser.json())
 
 
 app.get('/',async(req,res)=>{
@@ -17,7 +28,15 @@ app.get('/',async(req,res)=>{
 
 app.use(errorHandler);
 
+mongoose.connect(uri).then((result) => {
+    console.log('database connected')
+   
+
 app.listen(port,()=>{
 console.log(`app running on port number ${port}`)
 })
 
+
+}).catch((err) => {
+    
+});
